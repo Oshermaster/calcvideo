205b8b7b669f51712076748dd629020458b6ae96
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-11-02 22:34:09 +0200

    Updates

---
+++ precache-manifest.2bce5e04a4564c89f166622096815061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f516cde88abae03f0f4b70af4711517",
+    "url": "/calcvideo/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calcvideo/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "cb903636db7137bc8e73",
+    "url": "/calcvideo/static/css/main.4e0d8f75.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calcvideo/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "cb903636db7137bc8e73",
+    "url": "/calcvideo/static/js/main.81f85c46.chunk.js"
+  },
+  {
+    "revision": "1eb202a2bb3b2eaf7b26",
+    "url": "/calcvideo/static/js/runtime~main.636e98a0.js"
+  }
+]);
